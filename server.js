8c1fbb93d523d6a5426eab159162c2bbe79771c5
edgargoro25-blip <edgargoro25@gmail.com>
2026-02-02 15:22:40 -0500

    Initialize HTTP server with Socket.IO support
    Set up a basic HTTP server with Socket.IO for real-time communication.

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const http = require('http');
+const server = http.createServer();
+const io = require('socket.io')(server, {
+  cors: { origin: "*" }
+});
+
+const PORT = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  console.log('Jugador conectado');
+  
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+  });
+
+  socket.on('send-score', (data) => {
+    socket.to(data.roomId).emit('opponent-data', data);
+  });
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log('Servidor en puerto ' + PORT);
+});
 
